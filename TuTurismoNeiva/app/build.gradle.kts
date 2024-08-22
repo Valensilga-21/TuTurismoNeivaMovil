c7c25dbe53071b10a709101ce1b70e6d9d1cd8e7
Valentina Silva Garrido <valesilga21@gmail.com>
2024-08-22 13:53:45 -0500

    Se añadió el navegation bottom, inicio, mis sitios y ajustes

--- TuTurismoNeiva/app/build.gradle.kts
+++ TuTurismoNeiva/app/build.gradle.kts
@@ -26,6 +26,13 @@ android {
             )
         }
     }
+
+    android{
+        buildFeatures{
+            viewBinding = true
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
---
+++ TuTurismoNeiva/app/src/main/java/com/sena/tuturismoneiva/menu.kt
@@ -0,0 +1,52 @@
+package com.sena.tuturismoneiva
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.sena.tuturismoneiva.databinding.ActivityMenuBinding
+
+class menu : AppCompatActivity() {
+    private lateinit var binding : ActivityMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(inicio())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.inicio -> replaceFragment(inicio())
+                R.id.mis_sitios -> replaceFragment(mis_sitios())
+                R.id.ajustes -> replaceFragment(ajustes())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
+    }
+}
